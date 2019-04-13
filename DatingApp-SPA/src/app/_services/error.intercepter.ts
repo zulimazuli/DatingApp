0f503bbb72e9028f32ef4e8273022c415476b32b
zulimazuli <zulimazuli@gmail.com>
2019-04-13 18:59:29 +0200

    3rd party components added

--- DatingApp-SPA/src/app/_services/error.intercepter.ts
+++ DatingApp-SPA/src/app/_services/error.intercepter.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { type } from 'os';
+
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -21,13 +21,6 @@ export class ErrorInterceptor implements HttpInterceptor {
                     }
                     const serverError = error.error;
                     let modalStateErrors = '';
-                    // if (serverError && typeof serverError === 'object') {
-                    //     for (const key in serverError) {
-                    //         if(serverError[key]) {
-                    //             modelStateErrors += serverError[key] + '\n';
-                    //         }
-                    //     }
-                    // }
                     if (serverError.errors && typeof serverError.errors === 'object') {
                         for (const key in serverError.errors) {
                             if (serverError.errors[key]) {
